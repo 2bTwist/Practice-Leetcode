8a781a295f3fd180482db7951821521afaf1472c
2bTwist <ndanjiedmond@gmail.com>
2024-11-09 15:34:35 -0500

    Added Time Complexity

--- Array&Hashing/Contains Duplicate/ContainsDuplicate.py
+++ Array&Hashing/Contains Duplicate/ContainsDuplicate.py
@@ -1,6 +1,8 @@
 #Added this line to allow "List"
 from typing import List
 
+import time
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         #Brute Force
@@ -15,13 +17,24 @@ class Solution:
 if __name__ == "__main__":
     # Creating an instance of Solution
     solution = Solution()
-    
+
+    full_time = 0
+
     # Open and read file with test cases
     with open("test_cases.txt", "r") as file:
         for number, line in enumerate(file, 1):
             # map does integer casting on every value in each line
             nums = list(map(int, line.strip().split(",")))
 
+            start_time = time.perf_counter()
+
             result =  solution.hasDuplicate(nums)
+            
+            end_time = time.perf_counter()
+
+            full_time = full_time + (end_time-start_time)
 
             print(f"Case 1:{nums} = {result}")
+            print(f"Time Taken: {(end_time-start_time)* 10**6:.2f}\u00b5s \n")
+    
+    print(f"Total Time: {full_time* 10**6:.2f}\u00b5s")
