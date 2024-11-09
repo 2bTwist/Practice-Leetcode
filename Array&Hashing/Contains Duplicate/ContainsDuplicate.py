bbb19d7d90916a9614d5d79bb0405dcca52e3a68
2bTwist <ndanjiedmond@gmail.com>
2024-11-08 22:39:47 -0500

    Implemented Brute force method

--- Array&Hashing/Contains Duplicate/ContainsDuplicate.py
+++ Array&Hashing/Contains Duplicate/ContainsDuplicate.py
@@ -1,3 +1,13 @@
+#Added this line to allow "List"
+from typing import List
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        pass
+        #Brute Force
+        #Time Complexity: O(n^2)
+        #Space Complexity: O(1)
+        for i in range(0, len(nums)):
+            for j in range((i+1), len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False    
