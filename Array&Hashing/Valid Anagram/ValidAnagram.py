732aeba8edf156818d86d804e12bbc6e37b580c1
2bTwist <ndanjiedmond@gmail.com>
2024-11-09 17:45:14 -0500

    Revert "Updated Test Case File, Added main"
    This reverts commit fc1c74be2e6dc8ff73a4a3d1288562da5307baeb.

--- Array&Hashing/Valid Anagram/ValidAnagram.py
+++ Array&Hashing/Valid Anagram/ValidAnagram.py
@@ -18,36 +18,3 @@ class Solution:
                 return False
         
         return True
-
-if __name__ == "__main__":
-    # Creating an instance of Solution
-    solution = Solution()
-
-    full_time = 0
-    full_memory = 0
-
-    # Open and read file with test cases
-    with open("test_cases.txt", "r") as file:
-        for number, line in enumerate(file, 1):
-            # map does integer casting on every value in each line
-            nums = list(line.strip().split(","))
-
-            # Measure memory usage before running the function
-            initial_memory = sys.getsizeof(nums)  # Memory used by 'nums' list
-
-            start_time = time.perf_counter()
-
-            result =  solution.isAnagram(nums[0], nums[1])
-            
-            end_time = time.perf_counter()
-
-            # Measure memory usage after running the function
-            final_memory = sys.getsizeof(nums) + sys.getsizeof(result)  # Memory used by 'nums' and 'result'
-
-            full_time = full_time + (end_time-start_time)
-            full_memory = (final_memory) + full_memory
-
-            print(f"s:{nums[0]}, t:{nums[1]} = {result}")
-            print(f"Time Taken: {(end_time-start_time)* 10**6:.2f}\u00b5s, Memory Usage:{final_memory}bytes \n")
-    
-    print(f"***Total Time: {full_time* 10**6:.2f}\u00b5s, Total Memory:{full_memory}bytes")
