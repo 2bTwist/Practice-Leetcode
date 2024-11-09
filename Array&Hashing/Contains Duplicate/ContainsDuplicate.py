a8ba6305459443cced96b37825403d140ad2e68c
2bTwist <ndanjiedmond@gmail.com>
2024-11-09 15:54:07 -0500

    Added Space Complexity

--- Array&Hashing/Contains Duplicate/ContainsDuplicate.py
+++ Array&Hashing/Contains Duplicate/ContainsDuplicate.py
@@ -2,6 +2,7 @@
 from typing import List
 
 import time
+import sys
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
@@ -19,6 +20,7 @@ if __name__ == "__main__":
     solution = Solution()
 
     full_time = 0
+    full_memory = 0
 
     # Open and read file with test cases
     with open("test_cases.txt", "r") as file:
@@ -26,15 +28,22 @@ if __name__ == "__main__":
             # map does integer casting on every value in each line
             nums = list(map(int, line.strip().split(",")))
 
+            # Measure memory usage before running the function
+            initial_memory = sys.getsizeof(nums)  # Memory used by 'nums' list
+
             start_time = time.perf_counter()
 
             result =  solution.hasDuplicate(nums)
             
             end_time = time.perf_counter()
 
+            # Measure memory usage after running the function
+            final_memory = sys.getsizeof(nums) + sys.getsizeof(result)  # Memory used by 'nums' and 'result'
+
             full_time = full_time + (end_time-start_time)
+            full_memory = (final_memory) + full_memory
 
             print(f"Case 1:{nums} = {result}")
-            print(f"Time Taken: {(end_time-start_time)* 10**6:.2f}\u00b5s \n")
+            print(f"Time Taken: {(end_time-start_time)* 10**6:.2f}\u00b5s, Memory Usage:{final_memory}bytes \n")
     
-    print(f"Total Time: {full_time* 10**6:.2f}\u00b5s")
+    print(f"***Total Time: {full_time* 10**6:.2f}\u00b5s, Total Memory:{full_memory}bytes")
