90766fa4a9207d062a6bfeac4f09cfd2a3481e19
2bTwist <ndanjiedmond@gmail.com>
2024-11-09 19:38:16 -0500

    Valid Palindrome: Added main and sub-optimal solution

--- Two Pointers/Valid Palindrome/ValidPalindrome.py
+++ Two Pointers/Valid Palindrome/ValidPalindrome.py
@@ -2,31 +2,21 @@ import time
 import sys
 
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        #Edge Case: If s and t are not of thesame length of strings
-        if len(s) != len(t):
-            return False
-
+    def isPalindrome(self, s: str) -> bool:
+        
+        #Sub-Optimal
         # #Sub-Optimal
-        # #Time Complexity: O(nlogn) sorts in logn then compares each value for n
-        # #Space Complexity: O(1) or O(n) because of sorting algorithm
-        # return sorted(s) == sorted(t) #this is the solution(one line)
+        # #Time Complexity: O(n) because it goes through every character in s
+        # #Space Complexity: compares with a newly created, reverted string
+        pal = ""
 
-        #Optimal
-        #Time Complexity: O(n) adds each value in s and t too dictionaries, thus n
-        #Space Complexity: O(1)        
-        s_dict, t_dict = {}, {}
+        for i in s:
+            if i.isalnum():
+                pal = pal + i.lower()
+                # or pal += i.lower()
 
-        for i in range(len(s)):
-            s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
-            t_dict[t[i]] = 1 + t_dict.get(t[i], 0)
-
-        for key in s_dict:
-            if s_dict.get(key) != t_dict.get(key):
-                return False
-        # or simply
-        # return s_dict == t_dict
-        return True
+        #[::-1] is just a python method to revert strings
+        return pal == pal[::-1]
 
 if __name__ == "__main__":
     # Creating an instance of Solution
@@ -39,14 +29,14 @@ if __name__ == "__main__":
     with open("test_cases.txt", "r") as file:
         for number, line in enumerate(file, 1):
             # map does integer casting on every value in each line
-            nums = list(line.strip().split(","))
+            nums = line.strip()
 
             # Measure memory usage before running the function
             initial_memory = sys.getsizeof(nums)  # Memory used by 'nums' list
 
             start_time = time.perf_counter()
 
-            result =  solution.isAnagram(nums[0], nums[1])
+            result =  solution.isPalindrome(nums)
             
             end_time = time.perf_counter()
 
@@ -56,7 +46,7 @@ if __name__ == "__main__":
             full_time = full_time + (end_time-start_time)
             full_memory = (final_memory) + full_memory
 
-            print(f"s:{nums[0]}, t:{nums[1]} = {result}")
+            print(f"String:{nums} = {result}")
             print(f"Time Taken: {(end_time-start_time)* 10**6:.2f}\u00b5s, Memory Usage:{final_memory}bytes \n")
     
     print(f"***Total Time: {full_time* 10**6:.2f}\u00b5s, Total Memory:{full_memory}bytes")
